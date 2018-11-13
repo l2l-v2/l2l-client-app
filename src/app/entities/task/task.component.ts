a522f0a0d4b804c3f19c184ca15caa88542cf82a
iamcxnoguigan <747113809@qq.com>
2018-11-13 16:11:20 +0800

    task component

--- src/app/entities/task/task.component.ts
+++ src/app/entities/task/task.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Task, TaskQueryEntry} from './task.model';
+import {TaskDatasource} from './task.datasource';
+import {TaskService} from "./task.service";
 
 @Component({
   selector: 'app-task',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-
-  constructor() { }
+  private   tasks: task[];
+  dataSource: TaskDataSource;
+  displayedColumns = [ 'id', 'name' , 'owner' , 'parentTask' , 'priority' , 'processDefinition' , 'processInstance' , 'suspended' ];
+  total: number;
+  constructor(private taskService: TaskService ) { }
 
   ngOnInit() {
+    this.dataSource = new TaskDataSource(this.taskService);
   }
 
+  queryInstances(): void {
+    this.dataSource.queryProcessInstance(
+      this.sort.active,
+      this.sort.direction,
+      this.paginator.pageIndex,
+      this.paginator.pageSize);
+  }
+  // 需要根据datasource改写
+
+  complete(row: TaskQueryEntry): void {
+    this.taskService.complete(row.entry.id)
+      .subscribe(
+        (response) => {
+          const mockRes = <TaskQueryEntry>{
+            entry: {
+              id: row.entry.id,
+              processDefinitionId: row.entry.processDefinitionId,
+              taskDefinitionKey: row.entry.taskDefinitionKey,
+              assignee: row.entry.assignee,
+              status: 'RUNNING'
+            }
+          };
+          this.dataSource.update(mockRes);
+          console.log('POST call successful value returned in body');
+        },
+        (error) => {
+          console.log('POST call in error', error.message);
+        },
+        () => {
+          console.log('The POST observable is now completed.');
+        });
+  }
 }
